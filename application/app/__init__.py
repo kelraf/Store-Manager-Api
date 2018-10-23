b7cf522703b8c0a548514ace121e37689afbf397
Raphael Kingara <rafwambugu@gmail.com>
2018-10-23 17:07:54 +0300

    pycache

--- application/app/__init__.py
+++ application/app/__init__.py
@@ -14,7 +14,7 @@ def create_app(config_name):
 
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config["development"])
     app.register_blueprint(application_bp)
 
     api.add_resource(Products, "/products")
